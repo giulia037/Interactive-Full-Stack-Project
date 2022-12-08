321d2248ecc47c89e38a9c03232405507267207f
aaden9 <abdirisakaden9@gmail.com>
2022-12-08 14:46:03 -0500

    user login and routes created

---
+++ controllers/index.js
@@ -0,0 +1,9 @@
+const router = require('express').Router();
+
+const apiRoutes = require('./api');
+const userRoutes = require('./userRoutes.js');
+
+router.use('/', homeRoutes);
+router.use('/api', apiRoutes);
+
+module.exports = router;
