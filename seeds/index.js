afada91d5f16a39f2da4d774c23dbb94246cd222
aaden9 <abdirisakaden9@gmail.com>
2022-12-13 14:31:55 -0500

    working routes

--- seeds/index.js
+++ seeds/index.js
@@ -1,19 +1,21 @@
 const sequelize = require('../config/connection');
-// const seedHome = require('./homeData');
-// const seedBeginner = require('./beginnerData');
-// const seedIntermediate = require('./intermediateData');
+const seedHome = require('./homeData');
+const seedBeginner = require('./beginnerData');
+const seedIntermediate = require('./intermediateData');
 const seedAdvanced = require('./advancedData');
+const seedCollection = require('./collectionData');
 
 const seedAll = async () => {
   await sequelize.sync({ force: true });
 
   // await seedHome();
 
-  // await seedBeginner();
+  await seedBeginner();
 
-  // await seedIntermediate();
+  await seedIntermediate();
 
   await seedAdvanced();
+  await seedCollection();
 
   process.exit(0);
 };
