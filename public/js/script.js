edc142b2c359df4536245885c06512450bae5fd6
Lindsay <lindsayklug20@yahoo.com>
2022-12-13 21:38:00 -0500

    Moved script.js

---
+++ public/js/script.js
@@ -0,0 +1,54 @@
+var formEl = $('#guestbook-form');
+var nameInputEl = $('#name-input');
+var commentInputEl = $('#comment-input');
+var options = [];
+var guestBookDisplayEl = $('#guest-book-display');
+
+function openWindow(){
+  // create Window 
+  var options = [];
+  var namelink = window.prompt(" Which level are you ? ");
+  console.log(namelink);
+
+var printGuestData = function (name, comment) {
+  var cardColumnEl = $('<div>');
+  cardColumnEl.addClass('col-12 col-sm-4 col-md-3');
+
+  var cardEl = $('<div>');
+  // Add a class of .custom-card
+  cardEl.addClass('card h-100 custom-card');
+  cardEl.appendTo(cardColumnEl);
+
+  // Add a class of .custom-card-header
+  var cardName = $('<h5>').addClass('card-header custom-card-header').text(name);
+  cardName.appendTo(cardEl);
+
+  var cardBodyEl = $('<div>');
+  cardBodyEl.addClass('card-body');
+  cardBodyEl.appendTo(cardEl);
+
+  var cardComment = $('<p>').addClass('card-text').text(comment);
+  cardComment.appendTo(cardBodyEl);
+
+  guestBookDisplayEl.append(cardColumnEl);
+};
+
+var handleFormSubmit = function (event) {
+  event.preventDefault();
+
+  var nameInput = nameInputEl.val();
+  var commentInput = commentInputEl.val();
+
+  if (!nameInput || !commentInput) {
+    console.log('You need to fill out the form!');
+    return;
+  }
+
+  printGuestData(nameInput, commentInput);
+
+  // reset form
+  nameInputEl.val('');
+  commentInputEl.val('');
+};
+
+formEl.on('submit', handleFormSubmit); }
